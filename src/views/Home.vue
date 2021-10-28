62773cb26606f656e936441238bfc553f3286ec9
Rogister <rogisterzs@gmail.com>
2021-10-28 12:20:55 +0800

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,30 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <el-container style="height: 100%">
+<!--      <el-aside class="functionmenu"></el-aside>-->
+      <el-container>
+        <el-header>
+          SW登录
+        </el-header>
+        <el-main>
+          <el-button>登录</el-button>
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
-  },
+  components: {},
 };
 </script>
+<style lang="scss" scoped>
+.functionmenu{
+  width: 20%;
+  background-color: grey;
+}
+</style>
